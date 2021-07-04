c87dee7cb19a6a779c77c118bd567e0026285da4
Ashutosh-Maurya-87 <ashumaurya486@gmail.com>
2021-07-04 22:01:03 +0530

    counting_App

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react/cjs/react.production.min';
 
 function App() {
+  const count=0;
+  const [countNew,setCount]=useState(count)
+  const clickCount=()=>{
+    console.log('you click');
+  }
   return (
     <>
-    <h1>0</h1>
-    <button>Click</button>
+    <h1>{countNew}</h1>
+    <button onClick={clickCount}>Click</button>
     </>
   );
 }
